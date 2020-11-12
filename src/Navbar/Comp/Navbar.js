c7bb9d7d9d4df50bfa78b5f71a897306b8575b52
Nakarda Richards <nakardarichards3@gmail.com>
2020-11-12 00:58:22 -0500

    firebase-react

--- src/Navbar/Comp/Navbar.js
+++ src/Navbar/Comp/Navbar.js
@@ -63,7 +63,7 @@ export const Bars = styled(FaBars)`
 
 export const NavMenu = styled.div`
   display: flex;
-  align-items: center;
+  align-items: center;f
   margin-right: -24px;
 
   // @media screen and (max-width: 768px) {
